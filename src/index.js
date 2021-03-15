ac46f0d999ef8910c8a62677af50dd767470e01f
MishaKhmara <1927mishakhmara03@gmail.com>
2021-03-16 01:12:14 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@
  import manyCountry from './templates/country.hbs'
  import fetchCountries from './js/fetchCountries.js';
  import { error } from "@pnotify/core/";
+ import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+
  import getRefs from './js/get-refs'
  const debounce = require("lodash.debounce");
  const refs = getRefs();
    
- refs.inputRef.addEventListener('input', debounce(onInput, 1000));
+ refs.inputRef.addEventListener('input', debounce(onInput, 500));
 
   
     
@@ -23,7 +26,7 @@ function renderCountrycard(country){
         refs.countriesListRef.insertAdjacentHTML("beforeend", markup);
     }else{ error({
         text: "Too many matches found. Please enter a more specific query!",
-        delay: 3000,
+        
       });}
   
 }
@@ -32,7 +35,9 @@ function renderCountrycard(country){
     refs.countriesListRef.innerHTML = ""
     const form = event.target
     const inputValue = form.value;
- 
+     if(inputValue === 0){
+    refs.countriesListRef.innerHTML = ""
+     }
     return fetchCountries(inputValue).then(renderCountrycard)
     .catch(error => console.log(error))
    
