b455063aeb8c8216932244caa88fed150799d1af
MishaKhmara <1927mishakhmara03@gmail.com>
2021-03-16 16:48:10 +0200

    Исправил ошибки

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@
 import "@pnotify/core/dist/BrightTheme.css";
 
  import getRefs from './js/get-refs'
- const debounce = require("lodash.debounce");
+ import debounce from "lodash.debounce";
  const refs = getRefs();
    
  refs.inputRef.addEventListener('input', debounce(onInput, 500));
@@ -18,16 +18,20 @@ import "@pnotify/core/dist/BrightTheme.css";
     
 function renderCountrycard(country){
     refs.countriesListRef.innerHTML = "";
+   
+    
     if(country.length === 1){
         const markup = countriesTpl(country)
         refs.countriesListRef.insertAdjacentHTML("beforeend", markup);
     } else if (country.length > 1 && country.length <= 10 && country.length !== 0){
         const markup = manyCountry(country)
         refs.countriesListRef.insertAdjacentHTML("beforeend", markup);
-    }else{ error({
+    }else if (country.length > 10){ error({
         text: "Too many matches found. Please enter a more specific query!",
         
-      });}
+      });}else {error({
+        text: "matches not found !",
+         });}
   
 }
 
@@ -35,10 +39,10 @@ function renderCountrycard(country){
     refs.countriesListRef.innerHTML = ""
     const form = event.target
     const inputValue = form.value;
-     if(inputValue === 0){
-    refs.countriesListRef.innerHTML = ""
-     }
+    if(!inputValue){
+        return;
+    }
     return fetchCountries(inputValue).then(renderCountrycard)
-    .catch(error => console.log(error))
+    .catch({error})
    
     }
